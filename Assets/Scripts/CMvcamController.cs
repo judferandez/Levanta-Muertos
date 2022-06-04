59af8236aa17c177f1f83646e3a395a1500f2d94
Camilo <juan-camilo-lm@hotmail.com>
2022-06-04 00:51:17 -0500

    arreglo de la camara, se paso a que estuviera dentro del prefab

--- Assets/Scripts/CMvcamController.cs
+++ Assets/Scripts/CMvcamController.cs
@@ -7,12 +7,12 @@ public class CMvcamController : MonoBehaviour
 
     // Start is called before the first frame update
 
-    CinemachineVirtualCamera vcam;
+    //CinemachineVirtualCamera vcam;
     void Start()
     {
-        vcam = GetComponent<CinemachineVirtualCamera>();
+        /*vcam = GetComponent<CinemachineVirtualCamera>();
         vcam.LookAt = new Vector3(10,10,10);
-        vcam.Follow = new Vector3(10,10,10);
+        vcam.Follow = new Vector3(10,10,10);*/
     }
 
     // Update is called once per frame
